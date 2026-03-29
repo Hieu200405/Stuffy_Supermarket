88855b54f1c904f5a33fe85deaed949ec8241500
Do Hoang Hieu With Hangg <dohoanghieu7537@gmail.com>
2026-03-29 12:47:31 +0700

    fix(mfe): sửa lỗi Eager Consumption bằng Async Boundary

--- 3d-viewer-app/src/index.js
+++ 3d-viewer-app/src/index.js
@@ -1,7 +1,1 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Viewer from "./Viewer";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<Viewer />);
+import('./bootstrap');
